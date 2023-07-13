a9de9d84bd5a4e379314822d1f694ad645cda230
Kishor Krishna K <kishorkrishna3k@gmail.com>
2023-07-13 18:36:44 +0530

    fixed baseurl issue

--- client/src/pages/Story.jsx
+++ client/src/pages/Story.jsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from "../axios";
 import { useEffect, useState, useContext } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import bookmark from "../assets/images/bookmark2.svg";
